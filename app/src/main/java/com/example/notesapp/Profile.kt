d6686cbdf9ffb3ce1de60a4d37bf89035ceabda9
IamBansal <abansal11ae@gmail.com>
2022-02-04 21:47:32 +0530

    Database added

--- app/src/main/java/com/example/notesapp/Profile.kt
+++ app/src/main/java/com/example/notesapp/Profile.kt
@@ -2,10 +2,53 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Profile : AppCompatActivity() {
+
+    private var name : TextView? = null
+    private var phone : TextView? = null
+    private var about : TextView? = null
+    private var email : TextView? = null
+    private var firebaseAuth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        name = findViewById(R.id.nameP)
+        phone = findViewById(R.id.phoneP)
+        about = findViewById(R.id.aboutP)
+        email = findViewById(R.id.emailP)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        showProfile()
+
     }
+
+    //For displaying user's info
+    private fun showProfile() {
+        FirebaseDatabase.getInstance().reference.child("Users")
+            .child(firebaseAuth?.currentUser!!.uid).addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    name?.text = snapshot.child("Name").value.toString()
+                    email?.text = snapshot.child("Email").value.toString()
+                    phone?.text = snapshot.child("Phone Number").value.toString()
+                    about?.text = snapshot.child("About User").value.toString()
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+    }
+
 }
--- app/src/main/java/com/example/notesapp/Signup.kt
+++ app/src/main/java/com/example/notesapp/Signup.kt
@@ -1,22 +1,24 @@
 package com.example.notesapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class Signup : AppCompatActivity() {
 
     private var email : TextInputEditText? = null
     private var password : TextInputEditText? = null
     private var confirmPassword : TextInputEditText? = null
+    private var name : TextInputEditText? = null
+    private var phone : TextInputEditText? = null
+    private var about : TextInputEditText? = null
     private var signup : Button? = null
     private var firebaseAuth : FirebaseAuth? = null
 
@@ -31,6 +33,9 @@ class Signup : AppCompatActivity() {
         password = findViewById(R.id.passSign)
         confirmPassword = findViewById(R.id.passConfirmSign)
         signup = findViewById(R.id.btnSign)
+        name = findViewById(R.id.name)
+        phone = findViewById(R.id.phone)
+        about = findViewById(R.id.About)
         firebaseAuth = FirebaseAuth.getInstance()
 
         signup?.setOnClickListener {
@@ -50,11 +55,15 @@ class Signup : AppCompatActivity() {
         val emailText = email?.text.toString().trim()
         val passwordText = password?.text.toString().trim()
         val confirmPasswordText = confirmPassword?.text.toString().trim()
+        val nameText = name?.text.toString().trim()
+        val phoneText = phone?.text.toString().trim()
+        val aboutText = about?.text.toString().trim()
 
-        if (TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passwordText) || TextUtils.isEmpty(confirmPasswordText)) {
+        if (TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passwordText) || TextUtils.isEmpty(confirmPasswordText) ||
+            TextUtils.isEmpty(nameText) || TextUtils.isEmpty(phoneText) || TextUtils.isEmpty(aboutText)) {
             val alert = AlertDialog.Builder(this)
             alert.setTitle("Signup failed!!")
-                .setMessage("fill all credentials first.")
+                .setMessage("Fill all credentials first.")
                 .setPositiveButton("Okay"){_,_-> }
                 .create()
                 .show()
@@ -68,8 +77,24 @@ class Signup : AppCompatActivity() {
         } else {
             firebaseAuth?.createUserWithEmailAndPassword(emailText, passwordText)?.addOnCompleteListener { task ->
                 if(task.isSuccessful) {
-                    Toast.makeText(this, "Signed up successfully.\nLogin to continue.", Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this, LoginSignup::class.java))
+
+                    //Storing user's info
+                    val map = HashMap<String, Any>()
+                    map["Email"] = emailText
+                    map["Name"] = nameText
+                    map["Phone Number"] = phoneText
+                    map["About User"] = aboutText
+
+                    //Updating user's info to realtime database
+                    FirebaseDatabase.getInstance().reference.child("Users")
+                        .child(firebaseAuth?.currentUser!!.uid).updateChildren(map).addOnCompleteListener { task1 ->
+                            if (task1.isSuccessful) {
+                                Toast.makeText(this, "Signed up successfully.\nLogin to continue.", Toast.LENGTH_SHORT).show()
+                                startActivity(Intent(this, LoginSignup::class.java))
+                            } else {
+                                Toast.makeText(this, task1.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
                 } else {
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
                 }
